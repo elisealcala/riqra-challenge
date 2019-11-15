756ce1969ee3092edddc53df96b4f9dd5e50540a
Elizabeth Alcalá <elizabethalcala28@gmail.com>
2019-11-14 19:03:30 -0500

    Create product schema

--- server.js
+++ server.js
@@ -1,14 +1,40 @@
 const express = require('express');
 const app = express();
 const port = 4000;
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
 const bodyParser = require('body-parser');
 const cors = require('cors')
-const sequalize = require("./src/database/connection");
 const Product = require("./src/models/Product");
+
+
+const schema = buildSchema(`
+  type Product {
+    name: String,
+    brand: String,
+    image: String,
+    price: Int,
+  },
+  type Query {
+    products: [Product]
+  }
+`);
+
+const root = {
+  products: () => {
+    return Product.findAll().then(product => product);
+  },
+};
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
 
 app.listen(port, () => {
   sequelize
--- src/schema/index.js
+++ src/schema/index.js
@@ -3,20 +3,32 @@ import {
   GraphQLSchema,
   GraphQLObjectType,
   GraphQLString,
-} from 'graphql';
+  GraphQLInt,
+  GraphQLList
+} from "graphql";
 
-var schema = new GraphQLSchema({
+const productType = new GraphQLObjectType({
+  name: "Product",
+  fields: () => ({
+    name: { type: GraphQLString },
+    brand: { type: GraphQLString },
+    image: { type: GraphQLString },
+    price: { type: GraphQLInt },
+  })
+});
+const schema = new GraphQLSchema({
   query: new GraphQLObjectType({
-    name: 'RootQueryType',
-    fields: {
-      hello: {
-        type: GraphQLString,
-        resolve() {
-          return 'world';
-        },
-      },
+  name: "RootQueryType",
+  fields: {
+    product: {
+      type: productType,
+      resolve() {
+        return "asdasd";
+      }
     },
-  }),
+  }
+})
 });
 
+export { graphql };
 export default schema;
