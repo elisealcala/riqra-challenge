a21dda9011f1e2d7c748769e485e81e1a31ac3c1
Elizabeth Alcalá <elizabethalcala28@gmail.com>
2019-11-16 15:05:24 -0500

    Configuration

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,9 +1,10 @@
 const Sequelize = require("sequelize");
+const env = process.env.NODE_ENV || 'development';
+const config = require("../../config/config.json")[env];
 
-const sequelize = new Sequelize("riqra", "root", "Mobiles123!", {
-  host: "127.0.0.1",
-  dialect: "mysql",
-});
+console.log(config);
+
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 module.exports = sequelize;
 global.sequelize = sequelize;
